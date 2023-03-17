07da77719ed1f3b4197eb8f4dd37bdc6997ef7b3
Ian Scott Martin <ianscottmartin@gmail.com>
2023-03-17 11:51:03 -0600

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+//function divide(a, b) {
+//   return a / b
+//}
+
+//function square(a) {
+//    return a * a
+//}
+
+//function add(a, b) {
+//    return a + b
+
+//}
+
+
+// now make these into arrow functions
+
+let divide = (x, y) => x / y;
+let square = (x) => x * x;
+let add = (x, y) => x + y;
+
+
+
